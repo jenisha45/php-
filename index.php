785a40925bc3aea462d8e510cc6f2da2f4a14cfc
jenisha45 <jenishakharbuja0@gmail.com>
2026-08-25 19:44:04 +0545

    intial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+// This is a single-line comment
+# This is also a single-line comment
+
+/* This is a
+multi-line
+comment */
+$intro="This is an introduction to basic php";
+print "<h2>". $intro . "<h2>";
+print("print can be used with or without parenthesis()")."<br>";
+echo "print and echo is used to produce output"."<br>";
+$a=321;
+$b="jenisha kharbuja";
+echo $a. "<br>";
+echo $b. "<br>";
+
+var_dump(5);
+var_dump("John");
+var_dump(3.14);
+var_dump(true);
+var_dump([2, 3, 56]);
+var_dump(NULL);
+
+?>
