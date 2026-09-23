62ef897a4647525241a56744ae0f3519f239829a
jenisha45 <jenishakharbuja0@gmail.com>
2026-09-23 20:26:54 +0545

    Remove multiply function and adjust product output
    Removed the multiply function and updated calls to sum and multiply.

--- functiontype.php
+++ functiontype.php
@@ -18,9 +18,10 @@ function multiply($a, $b)
     $result= $a * $b;
     echo "The product of $a and $b is: $result<br>";
 }
-function multiply($a,$b,$c)
-{
-    $result= $a * $b * $c;
-    echo "The product of $a, $b and $c is: $result<br>";
-}
+greeting();
+$sum_result = add(10, 20);
+echo "The sum is: $sum_result<br>";
+$prod_result = multiply(5, 6);
+echo "The product is: $prod_result<br>";
+sum(15, 25);
 ?>
